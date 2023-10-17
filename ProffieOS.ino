1bd6d24ee14ca07b68a5fd2f28258d1e3a77ae94
Kim Nguyen <kim.nguyen1604@gmail.com>
2023-10-17 09:24:06 -0600

    Fix typo in ProffieOS.ino config define

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -25,7 +25,7 @@
 |  Add the slashes to any that you are not using.                   |
 \*-----------------------------------------------------------------*/
 
-#define CONFIG_FILE "proffieboard_v3_qgj_config.h"
+#define CONFIG_FILE "config/proffieboard_v3_qgj_config.h"
 
 // #define CONFIG_FILE "config/default_proffieboard_config.h"
 // #define CONFIG_FILE "config/default_v3_config.h"
